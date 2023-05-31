536885fb7698bb43a9028a52ccae25d9755f40e9
MartynK <banditamarci@gmail.com>
2023-05-31 18:14:55 +0200

    Added documentation
    ISSUE: error with functions.R availability

--- R/functions.r
+++ R/functions.r
@@ -7,6 +7,23 @@ require(gtsummary)
 
 #####
 # Defining my favorite ggplot theme with 'correct' caption placement
+
+
+#' Default Theme for ggplot
+#'
+#' This function applies a pre-specified theme to a ggplot object. The theme 
+#' is based on the 'theme_bw' function from the 'ggplot2' package and adds 
+#' some modifications. Specifically, it removes the legend, adjusts the plot 
+#' caption, and changes the font family to "serif".
+#' 
+#' @return A ggplot theme object that can be added to a ggplot.
+#' @export
+#' @examples
+#' \dontrun{
+#' library(ggplot2)
+#' p <- ggplot(mtcars, aes(mpg, cyl)) + geom_point()
+#' p + theme_default_ggplot()
+#' }
 theme_default_ggplot <- theme_bw() +
   theme( legend.position = "none",
          plot.caption = element_text( vjust = .0,
--- R/wrangling.r
+++ R/wrangling.r
@@ -5,6 +5,7 @@
 #Not strictly necessary, but adding here for standalone executability
 source(here::here("inst","functions","load_stuff.r")) 
 
+
 descriptor <- read_excel(here::here("inst","extdata","description.xlsx"), skip = 0)
 
 # make a list to be used as labels for 'labelled::'
@@ -43,3 +44,26 @@ data <- here::here("inst","extdata","Iris.xls") %>%
   mutate(
     mock_ID = runif(1, .1, 20) %>% round
   )
+
+
+#' Modified Iris Dataset
+#'
+#' This is the famous Iris dataset, often used for teaching and demonstrating
+#' machine learning algorithms. The dataset consists of measurements of 
+#' 150 iris flowers from three different species.
+#' 
+#' I've modified it to demonstrate basic dplyr style wrangling
+#'
+#' @format A data frame with 150 rows and 9 variables:
+#' \describe{
+#'   \item{species_no}{Numeric representation of species. It is a numeric vector of length 150 with no missing values and 3 distinct values.}
+#'   \item{petal_width}{These are the width of the petals. It is a numeric vector of length 150 with no missing values and 22 distinct values.}
+#'   \item{petal_length}{These are the length of the petals. It is a numeric vector of length 150 with no missing values and 43 distinct values.}
+#'   \item{sepal_width}{These are the width of the sepals. It is a numeric vector of length 150 with no missing values and 23 distinct values.}
+#'   \item{sepal_length}{These are the length of the sepals. It is a numeric vector of length 150 with no missing values and 35 distinct values.}
+#'   \item{species_char}{Character representation of the species. It is a factor with 150 levels and 3 distinct values.}
+#'   \item{date}{This is a date column to illustrate transformations. It is a Date vector of length 150 with no missing values and 150 distinct values.}
+#'   \item{newvar}{This is a new example variable, adding up the lengths. It is a numeric vector of length 150 with no missing values and 42 distinct values.}
+#'   \item{mock_ID}{It is a numeric vector of length 150 with no missing values and 21 distinct values.}
+#' }
+"data"
