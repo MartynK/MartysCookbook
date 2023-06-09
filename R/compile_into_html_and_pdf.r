2a974d2a455c4d7b5adaf1137057b9c4c9867fc0
MartynK <banditamarci@gmail.com>
2023-06-09 15:39:46 +0200

    finished compile_rmd_multiple_outputs()
    Done, documented, unit tested

--- R/compile_into_html_and_pdf.r
+++ R/compile_into_html_and_pdf.r
@@ -5,34 +5,89 @@ require(rmarkdown)
 require(pagedown)
 
 
-compile_rmd_multiple_outputs <- function( input_file) {
-
+#' Compile an R Markdown file to multiple outputs
+#'
+#' This function takes an R Markdown file as input and generates three outputs:
+#' an HTML document, a PDF-like HTML document using the pagedown package, and
+#' a PDF document. If generating the PDF document fails (e.g., if Chrome is not 
+#' installed), the function will still generate the PDF-like HTML document.
+#'
+#' @param input_file The path to the R Markdown file to be compiled.
+#' @param max_wait Max. waittime (minutes) until the pdf finishes printing. 
+#' Default = 10
+#'
+#' @return Invisibly returns NULL. The side effect of this function is that 
+#' it writes the compiled documents to disk.
+#' 
+#' @importFrom rmarkdown render
+#' @importFrom pagedown chrome_print
+#' @importFrom here here
+#' @importFrom base tryCatch
+#'
+#' @examples
+#' \dontrun{
+#' compile_rmd_multiple_outputs("path/to/your_file.Rmd")
+#' }
+#'
+#' @export
+compile_rmd_multiple_outputs <- function( input_file, max_wait = 10) {
+  
   bname <- basename(input_file)
   file_name <- substr(bname, 1, nchar(bname)-4)
-  
+  directory_name <- dirname(input_file)
   
   # Define the output files
-  output_file_html     <- here::here("inst", paste0(file_name,".html"))
-  output_file_pdf_like <- here::here("inst", paste0(file_name,"_pdflike.html"))
-  output_file_pdf      <- here::here("inst", paste0(file_name,".pdf"))
+  output_file_html     <- paste0(directory_name, "/", file_name, ".html")
+  output_file_pdf_like <- paste0(directory_name, "/", file_name,"_pdflike.html")
+  output_file_pdf      <- paste0(directory_name, "/", file_name,".pdf")
   
 
-  # Generate a PDF output using pagedown
+  # Generate a PDF output using pagedown 
+  
   tryCatch({
     # This also leaves the pdflike .html 
     pagedown::chrome_print(input_file, output = output_file_pdf)
+  
     # Renaming output, this will be the pdflike .html version
-    rename( output_file_html, output_file_pdf_like)
+    # Create a progress bar
+    pb <- progress::progress_bar$new(
+      format = "Waiting for file: [:bar] :elapsed s",
+      total = max_wait * 60,  # convert minutes to seconds
+      width = 60
+    )
+    
+    # Start waiting for the file
+    for(i in 1:(max_wait * 60)) {
+      if(file.exists(output_file_pdf)) {
+        message("File found!")
+        break
+      }
+      
+      # If file doesn't exist, wait for 1 second then update the progress bar
+      Sys.sleep(1)
+      pb$tick()
+    }
+    
+    pb$terminate()
+    
+    # If the file still doesn't exist after the maximum wait time, print a message
+    if(!file.exists(output_file_pdf)) {
+      error("PDF not found after waiting for ", max_wait, " minutes.")
+      render(input_file, "pagedown::thesis_paged", output_file = output_file_pdf_like)
+    }
   },
   error = function(e) {
-    # If above failes (due to no chrome for ex.), at least generate the .html
     render(input_file, "pagedown::thesis_paged", output_file = output_file_pdf_like)
-  })
+  }
+)
+  
+  file.rename( output_file_html, output_file_pdf_like)
+
   
   # Generate an HTML output
   render(input_file, "html_document", output_file = output_file_html)
 
 }
 
-input_file <- here::here("inst", "example-thesis.Rmd")
-compile_rmd_multiple_outputs(input_file)
+# input_file <- here::here("inst", "example_thesis", "example-thesis.Rmd")
+# compile_rmd_multiple_outputs(input_file)
